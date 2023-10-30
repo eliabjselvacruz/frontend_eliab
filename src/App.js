d4a1d00a60bcf09478ad457e7ec711bb3a69f83b
eliabjselvacruz <eliabjselvacruz51@gmail.com>
2023-10-30 11:23:45 -1000

    Creando componente de inicio de sesión y validación de acceso de usuarios.

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from './pages/Home';
 import About from './pages/About';
@@ -7,18 +7,27 @@ import Teacher from './pages/Teacher';
 import TeacherList from './pages/TeacherList';
 import Descuento from './pages/Descuento';
 import ListaDescuento from './pages/ListaDescuento';
+import GuardarImagen from './pages/GuardarImagen';
+import Galeria from './pages/Galeria';
+import Login from './pages/Login';
 
 function App() {
+
+  const [userRol, setUserRol] = useState('');
+  
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/customer" element={<Customer />} />
-        <Route path="/teacher" element={<Teacher />} />
-        <Route path="/teacherList" element={<TeacherList />} />
-        <Route path="/descuento" element={<Descuento />} />
-        <Route path="/listaDescuento" element={<ListaDescuento />} />
+        <Route path="/" element={<Login rol={userRol} setRol={setUserRol} />} />
+        <Route path="/home" element={<Home rol={userRol} />} />
+        <Route path="/about" element={<About rol={userRol} />} />
+        <Route path="/customer" element={<Customer rol={userRol} />} />
+        <Route path="/teacher" element={<Teacher rol={userRol} />} />
+        <Route path="/teacherList" element={<TeacherList rol={userRol} />} />
+        <Route path="/descuento" element={<Descuento rol={userRol} />} />
+        <Route path="/listaDescuento" element={<ListaDescuento rol={userRol} />} />
+        <Route path="/galeria" element={<Galeria rol={userRol} />} />
+        <Route path="/guardar-imagen" element={<GuardarImagen rol={userRol} />} />
       </Routes>
     </Router>
   );
--- src/components/Header.js
+++ src/components/Header.js
@@ -3,7 +3,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import { Navbar, Nav, Offcanvas, Button, NavDropdown, Container } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 
-function Header() {
+function Header({ rol }) {
   const [showMenu, setShowMenu] = useState(false);
 
   const toggleMenu = () => {
@@ -11,32 +11,205 @@ function Header() {
   };
 
   return (
+
     <div>
-      {/* Navbar principal */}
-      <Navbar className="navbar-color" variant="dark" expand="md">
-        <Container>
-          <Navbar.Brand href="#home">Eliab51</Navbar.Brand>
-          <Navbar.Toggle
-            aria-controls="basic-navbar-nav"
-            style={{ display: 'none' }}
-            className="d-sm-none d-xs-none"
-          />
-          <Navbar.Collapse id="basic-navbar-nav">
-            <Nav className="ml-auto">
+      {rol === 'admin' && (
+        <div>
+          {/* Navbar principal */}
+          <Navbar className="navbar-color" variant="dark" expand="md">
+            <Container>
+              <Navbar.Brand href="#home">Eliab51</Navbar.Brand>
+              <Navbar.Toggle
+                aria-controls="basic-navbar-nav"
+                style={{ display: 'none' }}
+                className="d-sm-none d-xs-none"
+              />
+              <Navbar.Collapse id="basic-navbar-nav">
+                <Nav className="ml-auto">
+
+                  <Nav.Link>
+                    <Link to="/" className="link-unstyled">Inicio</Link>
+                  </Nav.Link>
+
+                  <Nav.Link>
+                    <Link to="/guardar-imagen" className="link-unstyled">Guardar Imagen</Link>
+                  </Nav.Link>
+
+                  <Nav.Link>
+                    <Link to="/galeria" className="link-unstyled">Galeria</Link>
+                  </Nav.Link>
+
+                  <Nav.Link>
+                    <Link to="/about" className="link-unstyled">Informacion</Link>
+                  </Nav.Link>
+
+                  <NavDropdown title="Clientes" id="clientes">
+                    <NavDropdown.Item>
+                      <Link to="/customer" className="link-unstyled">Registrar Cliente</Link>
+                    </NavDropdown.Item>
+
+                    <NavDropdown.Item>
+                      <Link to="/actualizar-cliente" className="link-unstyled">Listar Clientes</Link>
+                    </NavDropdown.Item>
+                  </NavDropdown>
+
+                  <NavDropdown title="Docentes" id="docentes">
+                    <NavDropdown.Item>
+                      <Link to="/teacher" className="link-unstyled">Registrar Docente</Link>
+                    </NavDropdown.Item>
+
+                    <NavDropdown.Item>
+                      <Link to="/teacherList" className="link-unstyled">Listar Docentes</Link>
+                    </NavDropdown.Item>
+                  </NavDropdown>
+
+                  <NavDropdown title="Descuentos" id="descuentos">
+                    <NavDropdown.Item>
+                      <Link to="/descuento" className="link-unstyled">Registrar Descuento</Link>
+                    </NavDropdown.Item>
+
+                    <NavDropdown.Item>
+                      <Link to="/listaDescuento" className="link-unstyled">Listar Descuentos</Link>
+                    </NavDropdown.Item>
+                  </NavDropdown>
+
+                </Nav>
+              </Navbar.Collapse>
+              <Button
+                variant="outline-light"
+                onClick={toggleMenu}
+                className="d-md-none d-block"
+                aria-controls="basic-navbar-nav"
+                aria-expanded={showMenu ? 'true' : 'false'}
+              >
+                Menú
+              </Button>
+            </Container>
+          </Navbar>
+
+          {/* Menú lateral (Offcanvas) */}
+          <Offcanvas show={showMenu} onHide={toggleMenu} placement="start">
+            <Offcanvas.Header closeButton>
+              <Offcanvas.Title>Menú</Offcanvas.Title>
+            </Offcanvas.Header>
+            <Offcanvas.Body>
+              <Nav className="flex-column">
+
+                <Nav.Link>
+                  <Link to="/" className="link-unstyled">Inicio</Link>
+                </Nav.Link>
+
+                <Nav.Link>
+                  <Link to="/about" className="link-unstyled">About</Link>
+                </Nav.Link>
+
+                <NavDropdown title="Clientes" id="clientes">
+                  <NavDropdown.Item>
+                    <Link to="/customer" className="link-unstyled">Registrar Cliente</Link>
+                  </NavDropdown.Item>
+                  <NavDropdown.Item>
+                    <Link to="/actualizar-cliente" className="link-unstyled">Listar Clientes</Link>
+                  </NavDropdown.Item>
+                </NavDropdown>
+
+                <NavDropdown title="Docentes" id="docentes">
+                  <NavDropdown.Item>
+                    <Link to="/teacher" className="link-unstyled">Registrar Docente</Link>
+                  </NavDropdown.Item>
+
+                  <NavDropdown.Item>
+                    <Link to="/teacherList" className="link-unstyled">Listar Docentes</Link>
+                  </NavDropdown.Item>
+                </NavDropdown>
+
+                <NavDropdown title="Descuentos" id="descuentos">
+                  <NavDropdown.Item>
+                    <Link to="/descuento" className="link-unstyled">Registrar Descuento</Link>
+                  </NavDropdown.Item>
+
+                  <NavDropdown.Item>
+                    <Link to="/listaDescuento" className="link-unstyled">Listar Descuentos</Link>
+                  </NavDropdown.Item>
+                </NavDropdown>
+
+              </Nav>
+            </Offcanvas.Body>
+          </Offcanvas>
+        </div>
+      )}
+
+      {rol === 'vendedor' && (
+        <div>
+        {/* Navbar principal */}
+        <Navbar className="navbar-color" variant="dark" expand="md">
+          <Container>
+            <Navbar.Brand href="#home">Eliab51</Navbar.Brand>
+            <Navbar.Toggle
+              aria-controls="basic-navbar-nav"
+              style={{ display: 'none' }}
+              className="d-sm-none d-xs-none"
+            />
+            <Navbar.Collapse id="basic-navbar-nav">
+              <Nav className="ml-auto">
+
+                <Nav.Link>
+                  <Link to="/" className="link-unstyled">Inicio</Link>
+                </Nav.Link>
+
+                <NavDropdown title="Clientes" id="clientes">
+                  <NavDropdown.Item>
+                    <Link to="/customer" className="link-unstyled">Registrar Cliente</Link>
+                  </NavDropdown.Item>
+
+                  <NavDropdown.Item>
+                    <Link to="/actualizar-cliente" className="link-unstyled">Listar Clientes</Link>
+                  </NavDropdown.Item>
+                </NavDropdown>
+
+                <NavDropdown title="Descuentos" id="descuentos">
+                  <NavDropdown.Item>
+                    <Link to="/descuento" className="link-unstyled">Registrar Descuento</Link>
+                  </NavDropdown.Item>
+
+                  <NavDropdown.Item>
+                    <Link to="/listaDescuento" className="link-unstyled">Listar Descuentos</Link>
+                  </NavDropdown.Item>
+                </NavDropdown>
+
+              </Nav>
+            </Navbar.Collapse>
+            <Button
+              variant="outline-light"
+              onClick={toggleMenu}
+              className="d-md-none d-block"
+              aria-controls="basic-navbar-nav"
+              aria-expanded={showMenu ? 'true' : 'false'}
+            >
+              Menú
+            </Button>
+          </Container>
+        </Navbar>
+
+        {/* Menú lateral (Offcanvas) */}
+        <Offcanvas show={showMenu} onHide={toggleMenu} placement="start">
+          <Offcanvas.Header closeButton>
+            <Offcanvas.Title>Menú</Offcanvas.Title>
+          </Offcanvas.Header>
+          <Offcanvas.Body>
+            <Nav className="flex-column">
 
               <Nav.Link>
                 <Link to="/" className="link-unstyled">Inicio</Link>
               </Nav.Link>
 
               <Nav.Link>
-                <Link to="/about" className="link-unstyled">Informacion</Link>
+                <Link to="/about" className="link-unstyled">About</Link>
               </Nav.Link>
 
               <NavDropdown title="Clientes" id="clientes">
                 <NavDropdown.Item>
                   <Link to="/customer" className="link-unstyled">Registrar Cliente</Link>
                 </NavDropdown.Item>
-
                 <NavDropdown.Item>
                   <Link to="/actualizar-cliente" className="link-unstyled">Listar Clientes</Link>
                 </NavDropdown.Item>
@@ -63,68 +236,13 @@ function Header() {
               </NavDropdown>
 
             </Nav>
-          </Navbar.Collapse>
-          <Button
-            variant="outline-light"
-            onClick={toggleMenu}
-            className="d-md-none d-block"
-            aria-controls="basic-navbar-nav"
-            aria-expanded={showMenu ? 'true' : 'false'}
-          >
-            Menú
-          </Button>
-        </Container>
-      </Navbar>
+          </Offcanvas.Body>
+        </Offcanvas>
+        </div>
 
-      {/* Menú lateral (Offcanvas) */}
-      <Offcanvas show={showMenu} onHide={toggleMenu} placement="start">
-        <Offcanvas.Header closeButton>
-          <Offcanvas.Title>Menú</Offcanvas.Title>
-        </Offcanvas.Header>
-        <Offcanvas.Body>
-          <Nav className="flex-column">
+      )}
+   </div>
 
-            <Nav.Link>
-              <Link to="/" className="link-unstyled">Inicio</Link>
-            </Nav.Link>
-
-            <Nav.Link>
-              <Link to="/about" className="link-unstyled">About</Link>
-            </Nav.Link>
-
-            <NavDropdown title="Clientes" id="clientes">
-              <NavDropdown.Item>
-                <Link to="/customer" className="link-unstyled">Registrar Cliente</Link>
-              </NavDropdown.Item>
-              <NavDropdown.Item>
-                <Link to="/actualizar-cliente" className="link-unstyled">Listar Clientes</Link>
-              </NavDropdown.Item>
-            </NavDropdown>
-
-            <NavDropdown title="Docentes" id="docentes">
-              <NavDropdown.Item>
-                <Link to="/teacher" className="link-unstyled">Registrar Docente</Link>
-              </NavDropdown.Item>
-
-              <NavDropdown.Item>
-                <Link to="/teacherList" className="link-unstyled">Listar Docentes</Link>
-              </NavDropdown.Item>
-            </NavDropdown>
-
-            <NavDropdown title="Descuentos" id="descuentos">
-              <NavDropdown.Item>
-                <Link to="/descuento" className="link-unstyled">Registrar Descuento</Link>
-              </NavDropdown.Item>
-
-              <NavDropdown.Item>
-                <Link to="/listaDescuento" className="link-unstyled">Listar Descuentos</Link>
-              </NavDropdown.Item>
-            </NavDropdown>
-
-          </Nav>
-        </Offcanvas.Body>
-      </Offcanvas>
-    </div>
   );
 }
 
